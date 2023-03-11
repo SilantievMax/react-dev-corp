8827e3528783f3e41441fdd9d66aa268889475ce
SilantievMax <nohrin.max@yandex.ru>
2023-03-11 20:33:55 +0300

    added banner

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,16 @@ import Footer from './components/Footer/Footer.jsx'
 import Header from './components/Header/Header.jsx'
 import 'normalize.css'
 import Layout from './components/Layout/Layout.jsx'
+import Banner from './components/Banner/Banner.jsx'
 
 const App = () => {
   return (
     <>
       <Layout>
         <Header />
-        <main></main>
+        <main>
+          <Banner />
+        </main>
         {/* <Footer /> */}
       </Layout>
     </>
